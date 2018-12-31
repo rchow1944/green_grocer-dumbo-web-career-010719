91b77ab014711be7320691e63ce340622f98fc05
Raymond Chow <raychow1944@yahoo.com>
2018-12-31 07:12:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,19 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+  cart_hash = {}
+  cart.each do |item|
+    item.each do |item_name, value|
+      if cart_hash[item_name] == nil
+        cart_hash[item_name] = value
+        cart_hash[item_name][:count] = 1 
+      elsif cart_hash[item_name] != nil
+        cart_hash[item_name]
+      end
+    end
+    binding.pry
+  end
 end
 
 def apply_coupons(cart, coupons)
